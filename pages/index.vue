eb40801d1fd8c85d02e19dd626244cbd818740bb
nobuaki.yamaji <nobuaki.yamaji@monopos.co.jp>
2020-09-24 19:40:13 +0900

    init

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  async asyncData() {
+    const { data } = await axios.get(
+      "https://jw0924.microcms.io/api/v1/blog",
+      {
+        headers: { "X-API-KEY": "a2679751-05f0-4365-8459-3c1da53ada00" },
+      }
+    );
+    return data;
+  },
+};
+</script>
